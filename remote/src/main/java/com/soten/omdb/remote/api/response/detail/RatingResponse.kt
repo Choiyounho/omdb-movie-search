a47087f0664260b6a96814f4f7116e05f7cbf447
Choiyounho <maxosa78@gmail.com>
2024-05-24 17:57:51 +0900

    refactor: Rating -> RatingResponse 네이밍 변경

--- remote/src/main/java/com/soten/omdb/remote/api/response/detail/RatingResponse.kt
+++ remote/src/main/java/com/soten/omdb/remote/api/response/detail/RatingResponse.kt
@@ -5,7 +5,7 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class Rating(
+data class RatingResponse(
     @SerialName("Source")
     val source: String,
     @SerialName("Value")
