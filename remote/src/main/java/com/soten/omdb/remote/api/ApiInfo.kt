f80d416abfb51f8ed08f14fefdbbbe8edcb05bd1
Choiyounho <maxosa78@gmail.com>
2024-05-24 17:36:56 +0900

    feat: ApiInfo

---
+++ remote/src/main/java/com/soten/omdb/remote/api/ApiInfo.kt
@@ -0,0 +1,1 @@
+package com.soten.omdb.remote.apiobject ApiInfo {    const val BASE_URL = "https://openapi.naver.com/v1/search/"    const val API_KEY = "665ada14"    object ApiEndPoints {        const val OMDB = "?"    }}
